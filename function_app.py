2e9eac2d3a50febbda714b89ed0f50f1eabfd3c8
andersonpaul <andersonudokoro@yahoo.com>
2026-04-22 10:45:13 +0100

    function app update

--- function_app.py
+++ function_app.py
@@ -1,7 +1,43 @@
 import azure.functions as func
+from kaggle.api.kaggle_api_extended import KaggleApi
+from azure.storage.filedatalake import DataLakeServiceClient
+from datetime import datetime
+import os
 
 app = func.FunctionApp()
 
 @app.route(route="run-pipeline", auth_level=func.AuthLevel.ANONYMOUS)
 def run_pipeline(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("WORKING")
+    today = datetime.now().strftime("%Y%m%d")
+
+    account_name = "bigdataeducation"
+    account_key = os.getenv("ACCOUNT_KEY")
+    file_system_name = "dataset"
+
+    local_file_path = "./data/Life Expectancy Data.csv"
+    target_path = f"{today}/led_{today}.csv"
+
+    os.environ["KAGGLE_USERNAME"] = os.getenv("KAGGLE_USERNAME")
+    os.environ["KAGGLE_KEY"] = os.getenv("KAGGLE_KEY")
+    
+    api = KaggleApi()
+    api.authenticate()
+
+    api.dataset_download_files(
+        "kumarajarshi/life-expectancy-who",
+        path="./data",
+        unzip=True
+    )
+
+    service_client = DataLakeServiceClient(
+        account_url=f"https://{account_name}.dfs.core.windows.net",
+        credential=account_key
+    )
+
+    file_system_client = service_client.get_file_system_client(file_system_name)
+    file_client = file_system_client.get_file_client(target_path)
+
+    with open(local_file_path, "rb") as f:
+        file_client.upload_data(f, overwrite=True)
+
+    return func.HttpResponse("Upload complete", status_code=200)
